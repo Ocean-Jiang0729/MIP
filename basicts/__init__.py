cb380750943e0a5d94cf984e057742b489e4388a
Ocean-Jiang0729 <uqhjian5@uq.edu.au>
2024-12-07 15:58:52 +1000

    Delete basicts directory

--- basicts/__init__.py
+++
@@ -1,6 +0,0 @@
-from .launcher import launch_training, launch_runner
-from .runners import BaseRunner
-
-__version__ = "0.3.11"
-
-__all__ = ["__version__", "launch_training", "launch_runner", "BaseRunner"]
